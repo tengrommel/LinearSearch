60836aa2ea013db62edabea5fdf16942e8bbef10
teng <wwwsousoucom@126.com>
2020-10-21 13:23:07 +0800

    泛型

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -1,9 +1,17 @@
-public class LinearSearch {
+public class LinearSearch<E> {
     private LinearSearch(){}
-    public static int search(int[] data, int target) {
+    public static <E> int search(E[] data, E target) {
         for (int i = 0; i < data.length; i++)
-            if (data[i] == target)
+            if (data[i].equals(target))
                 return i;
         return -1;
+    }
+    // 泛型只能适用于类对象，不能适用于基本类型
+    public static void main(String[] args) {
+        Integer[] data = {24, 18, 12, 9, 16, 66, 32, 4};
+        int res = LinearSearch.search(data, 16);
+        System.out.println(res);
+        int res2 = LinearSearch.search(data, 666);
+        System.out.println(res2);
     }
 }
